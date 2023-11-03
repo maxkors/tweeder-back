e3f2a2ec287a3e1e5f036e2adfda2115dcadd2f3
Max <maxkorsv@gmail.com>
2023-11-03 19:49:22 +0100

    initial commit

---
+++ src/main/java/com/maxkors/tweeder/TweederApplication.java
@@ -0,0 +1,13 @@
+package com.maxkors.tweeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxkors/tweeder/TweederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxkors.tweeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
