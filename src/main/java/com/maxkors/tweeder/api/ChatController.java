84c701cf6e15c33b41060a09903f5dab00405bdc
Max <maxkorsv@gmail.com>
2024-03-24 22:13:09 +0100

    add WS auth

--- src/main/java/com/maxkors/tweeder/api/ChatController.java
+++ src/main/java/com/maxkors/tweeder/api/ChatController.java
@@ -1,17 +1,30 @@
 package com.maxkors.tweeder.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
+
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class ChatController {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/chat/sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage message) {
+    @SendToUser("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage message, Principal principal) {
+        message.setSender(principal.getName());
+        simpMessagingTemplate.convertAndSendToUser("commodus", "/topic/public", message);
+        log.info("@@@ USER: " + principal.getName());
         return message;
     }
 
--- src/main/java/com/maxkors/tweeder/config/WebSocketConfig.java
+++ src/main/java/com/maxkors/tweeder/config/WebSocketConfig.java
@@ -1,14 +1,31 @@
 package com.maxkors.tweeder.config;
 
+import com.maxkors.tweeder.security.JwtService;
+import com.sun.security.auth.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtService jwtService;
 
 //    @Override
 //    public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -30,5 +47,27 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    List<String> authorization = accessor.getNativeHeader("Authentication");
+                    log.debug("X-Authorization: {}", authorization);
+
+                    String accessToken = authorization.get(0).split(" ")[1];
+                    String username = jwtService.extractUsername(accessToken);
+//                    Jwt jwt = jwtDecoder.decode(accessToken);
+//                    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//                    Authentication authentication = converter.convert(jwt);
+                    accessor.setUser(new UserPrincipal(username));
+                }
+                return message;
+            }
+        });
     }
 }
