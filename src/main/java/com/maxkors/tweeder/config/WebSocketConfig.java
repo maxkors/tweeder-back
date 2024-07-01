941dd9ac2ed43747adbfa22fb8f67aaf7a3d5eb0
Max <maxkorsv@gmail.com>
2024-07-01 20:28:28 +0200

    fix WS urls for local dev

--- src/main/java/com/maxkors/tweeder/config/WebSocketConfig.java
+++ src/main/java/com/maxkors/tweeder/config/WebSocketConfig.java
@@ -40,7 +40,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:8080");
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:8080", "http://192.168.0.73:3000");
     }
 
     @Override
